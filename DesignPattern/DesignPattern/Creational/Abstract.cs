c9bf04b6f7f6b6d4f5d3641ba8710c829a2f428e
praveent696 <parveen.kumar@inficaretech.com>
2018-03-28 04:29:32 +0530

    initial Commit

---
+++ DesignPattern/DesignPattern/Creational/Abstract.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern.Creational
+{
+    abstract class FoodFactory
+    {
+        public abstract Veg createveg();
+        public abstract NonVeg createnonveg();
+    }
+
+    class Factory1 : FoodFactory
+    {
+        public override Veg createveg()
+        {
+            return new Sabzi();
+        }
+        public override NonVeg createnonveg()
+        {
+            return new Chicken();
+        }
+    }
+
+    class Factory2 : FoodFactory
+    {
+        public override Veg createveg()
+        {
+            return new Roti();
+        }
+        public override NonVeg createnonveg()
+        {
+            return new Briyani();
+        }
+    }
+
+    abstract class Veg
+    {
+
+    }
+    abstract class NonVeg
+    {
+        public abstract void comparision(Veg v);
+    }
+    class Roti : Veg
+    {
+
+    }
+    class Sabzi : Veg
+    {
+
+    }
+    class Briyani : NonVeg
+    {
+        public override void comparision(Veg v)
+        {
+            Console.WriteLine(v.GetType().Name + " is better than " + this.GetType().Name + " so veg is recommended");
+        }
+    }
+    class Chicken : NonVeg
+    {
+        public override void comparision(Veg v)
+        {
+            Console.WriteLine(v.GetType().Name + " is better than " + this.GetType().Name + " so veg is recommended");
+        }
+    }
+    class Client
+    {
+        private Veg _veg;
+        private NonVeg _nonveg;
+
+
+        public Client(FoodFactory Ff)
+        {
+
+            _veg = Ff.createveg();
+            _nonveg = Ff.createnonveg();
+
+        }
+
+        public void comparefood()
+        {
+            _nonveg.comparision(_veg);
+        }
+    }
+}
---
+++ DesignPattern/DesignPattern/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Abstract factory
+
+            ////fact1
+            //Creational.FoodFactory fac1 = new Creational.Factory1();
+            //Creational.Client c = new Creational.Client(fac1);
+            //c.comparefood();
+
+            ////fact2
+            //Creational.FoodFactory fac2 = new Creational.Factory2();
+            //c = new Creational.Client(fac2);
+            //c.comparefood();
+
+            #endregion
+
+            Console.ReadKey();
+        }
+    }
+}
